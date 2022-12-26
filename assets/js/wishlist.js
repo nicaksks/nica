8d75de1b6f4444686ea6200c015ea3e42f64c910
nica <100009274+nicaksks@users.noreply.github.com>
2022-12-25 22:31:12 -0300

    Update wishlist.js

--- assets/js/wishlist.js
+++ assets/js/wishlist.js
@@ -11,10 +11,7 @@ async function t() {
 
   const data = await wishlist();
 
-  //Name, photo, price, category and url;
-
   for(i in data) {
-    //products.innerHTML += `<a href="${data[i].url}" target="_blank"><button class="products">${data[i].name} <br><span style="color: pink">Preço: R$${data[i].price}</span></button><br></a>`;
     products.innerHTML += `<a href="${data[i].url}" target="_blank"><button class="products"><img src=${data[i].photo}> <br><span>${data[i].name}</span> <br>Preço: R$${data[i].price}</button><br></a>`; 
   };
 };

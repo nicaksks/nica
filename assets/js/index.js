f8dfe3841b7cd770c518cbbe51db8c05b46acf85
nica <100009274+nicaksks@users.noreply.github.com>
2023-02-06 12:05:21 -0300

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,7 +20,7 @@ const imgs = {
 
 async function getInfo() {
   const r = await axios.get(`https://api.github.com/users/${username}`)
-  .then(response => response.data);
+    .then(response => response.data);
 
   avatar.src = r.avatar_url;
   nickname.innerHTML = r.name;
@@ -34,18 +34,19 @@ async function getRepos() {
   getInfo();
 
   const r = await axios.get(`https://api.github.com/users/${username}/repos?sort=created`)
-  .then(response => response.data);
+    .then(response => response.data);
 
   let p;
   r.length > n ? p = n : p = r.length;
 
-  for(i=0; i < p; i++) {
+  for (i = 0; i < p; i++) {
 
     let = { html_url, full_name, description, language, stargazers_count, forks_count, pushed_at } = r[i];
 
     //Descrição;
-    description === null ? description = "Sem descrição" : description;
-    repos.innerHTML += `<div class="repos"><p><iconify-icon icon="octicon:repo-16"></iconify-icon> <a href="${html_url}" target="_blank">${full_name}<a/> <br>L ${description.substring(0, 50)} <br></iconify-icon> <img src=${imgs[language]}> <iconify-icon icon="octicon:star-fill-24" style="color: #DAAA3F"></iconify-icon> ${stargazers_count} <iconify-icon icon="octicon:repo-forked-16" style="color: gray"></iconify-icon> ${forks_count} <br>Atualizado ${checkDate(pushed_at)}</p></div>`;
+    description === null ? description = "Sem descrição." : description;
+
+    repos.innerHTML += `<div class="repos"><p><iconify-icon icon="octicon:repo-16"></iconify-icon> <a href="${html_url}" target="_blank">${full_name}<a/> <br>L ${textLength(description)} <br></iconify-icon> <img src=${imgs[language]}> <iconify-icon icon="octicon:star-fill-24" style="color: #DAAA3F"></iconify-icon> ${stargazers_count} <iconify-icon icon="octicon:repo-forked-16" style="color: gray"></iconify-icon> ${forks_count} <br>Atualizado ${checkDate(pushed_at)}</p></div>`;
   };
 };
 
@@ -59,22 +60,35 @@ function date() {
 
   date = yyyy + mm + dd;
 
-  return date;   
+  return date;
 };
 
 function checkDate(pushed_at) {
-    var u = new Date(pushed_at).toISOString().replace('-', '/').split('T')[0].replace('-', '/');
-    var d1 = new Date(u);
-    var d2 = new Date(date());
-    var dd = parseInt((d2 - d1) / (1000 * 60 * 60 * 24));
+  var u = new Date(pushed_at).toISOString().replace('-', '/').split('T')[0].replace('-', '/');
+  var d1 = new Date(u);
+  var d2 = new Date(date());
+  var dd = parseInt((d2 - d1) / (1000 * 60 * 60 * 24));
 
-    if(dd <= 0) {
-      dd = `<span style="color: #1DA1F2">algumas minutos</span> atrás.`
-    } else if (dd === 1) {
-      dd = `<span style="color: #1DA1F2">${dd}</span> dia atrás.`
-    } else if (dd >= 2) {
-      dd = `<span style="color: #1DA1F2">${dd}</span> dias atrás.`
-    };
+  if (dd <= 0) {
+    dd = `<span style="color: #1DA1F2">algumas minutos</span> atrás.`
+  } else if (dd === 1) {
+    dd = `<span style="color: #1DA1F2">${dd}</span> dia atrás.`
+  } else if (dd >= 2) {
+    dd = `<span style="color: #1DA1F2">${dd}</span> dias atrás.`
+  };
 
-    return dd
+  return dd;
+}
+
+function textLength(description) {
+
+  let text = "";
+
+  if(description.length >= 50) {
+    text = description.substring(0, 50) + "..."
+  } else {
+    text = description;
+  };
+
+  return text;
 }
